9ccea4f4b01991f08a2e3ffb0a9c021622c5647f
main <claudia.raphael73@gmail.com>
2025-02-26 18:02:40 -0300

    rename research_company to financial_research

--- source/financial_analyst_crew/config/crew.py
+++ source/financial_analyst_crew/config/crew.py
@@ -9,7 +9,28 @@ class FinancialAnalystCrew():
 
     # load up the agents and tasks you set up
     agents_config = 'config/agents.yaml'
-    task_config = 'config/task.yaml'
+    tasks_config = 'config/task.yaml'
 
-def __init__(self) -> None:
-    self.groq_llm = ChatGroq(temperature=0, model_name = "mixtral-8x7b-32768")
+    def __init__(self) -> None:
+        self.groq_llm = ChatGroq(temperature=0, model_name = "mixtral-8x7b-32768")
+
+    @agent
+    def financial_researcher(self) -> Agent:
+        return Agent(
+            config = self.agents_config['financial_researcher'],
+            llm = self.groq_llm
+        )
+     @agent
+    def financial_analyst(self) -> Agent:
+        return Agent(
+            config = self.agents_config['financial_analyst'],
+            llm = self.groq_llm
+        )
+    
+    @task
+    def research_company(self) -> Task:
+        return Task(
+            config = self.tasks_config['financial_research'],
+            agent = self.financial_researcher()
+
+        )
