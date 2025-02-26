834bc63d2523910185b0fe2f319d3d7254bf1b40
main <claudia.raphael73@gmail.com>
2025-02-26 17:48:26 -0300

    add Financial Analyst Crew; add groq_llm with Mixtral

--- source/financial_analyst_crew/config/crew.py
+++ source/financial_analyst_crew/config/crew.py
@@ -1,3 +1,15 @@
-from crewai import Agent, Crew, Process, Task
+from crewai import Agent, Crew, Process, Task # 4 main pillars of a crewai process. + Teams?
 from crewai.project import CrewBase, Agent, crew, task
 from langchain_groq import ChatGroq
+
+@CrewBase
+class FinancialAnalystCrew():
+    # describe what it is
+    """"FinancialAnalystCrew crew"""
+
+    # load up the agents and tasks you set up
+    agents_config = 'config/agents.yaml'
+    task_config = 'config/task.yaml'
+
+def __init__(self) -> None:
+    self.groq_llm = ChatGroq(temperature=0, model_name = "mixtral-8x7b-32768")
